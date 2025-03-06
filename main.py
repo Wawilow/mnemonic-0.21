d73a883e7b51f994b7752e87676dbea7db983401
George Wawilow <wawilow@protonmail.com>
2025-03-06 18:42:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ def to_eth_seed(mnemonic: str, passphrase: str = "") -> bytes:
     return stretched[:32]
 
 
+def to_ton_seed(mnemonic: str, passphrase: str = "") -> bytes:
+    passphrase = "mnemonic" + passphrase
+    mnemonic_bytes = mnemonic.encode("utf-8")
+    passphrase_bytes = passphrase.encode("utf-8")
+    stretched = hashlib.pbkdf2_hmac(
+        "sha512", mnemonic_bytes, passphrase_bytes, PBKDF2_ROUNDS
+    )
+    return stretched[:128]
+
+
 def to_btc_seed(mnemonic: str, passphrase: str = "") -> str:
     passphrase = "" + passphrase
     mnemonic_bytes = mnemonic.encode("utf-8")
@@ -51,3 +61,8 @@ btc_wif = to_btc_seed(seed)
 print("btc wif: ", btc_wif)
 # result
 # 5KfSmEhzpWHFP5DMzbxMxNr45tUXehebZufCzFXQpSiFRC7hwo7
+
+ton_pk = to_ton_seed(seed).hex()
+print("ton: ", ton_pk)
+# result
+# c5d5412349d66733f3beef726f2932290e711fd541d55c564030808365584ae357026a4b234d9b8f2be22c10c181f33494503f88af3573f21572006fb49ea8d1
